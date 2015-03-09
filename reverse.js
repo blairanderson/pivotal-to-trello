d55b99738394e2ca6348dfc0e8b27ff488e4ae24
Blair Anderson <blair@repairshopr.com>
2015-03-09 15:44:14 -0700

    more cleanup

--- reverse.js
+++ reverse.js
@@ -4,29 +4,77 @@ var request = require('request');
 var async = require('async');
 var Trello = require('node-trello');
 
-var project = "1295806";
-function createStory(story, callback){
-  return request({
+// MAP TRACKER STATUS TO TRELLO LIST
+// accepted, delivered, finished, started, rejected, planned, unstarted, unscheduled
+var listsToTracker = {
+  // status: idList
+
+  //'unstarted': '546b4c23ac36eb3d928725cf', // Backlog
+  // 'started': '5367a06afc26005e60d1e763', // in process
+  // 'finished': '5367a0d620609dc039ac3a63', // PR sent
+};
+
+// Migrate one board at a time, to make sure they're in the right place
+var current_state = 'unstarted';
+
+// TRELLO
+// visit("https://trello.com/app-key")
+var trelloCreds = {
+  boardId: null,
+  key: null,
+  token: null
+};
+
+
+// TRACKER
+var trackerProjectId = null;
+var trackerToken = null;
+
+
+[trelloCreds.boardId, trelloCreds.token, trelloCreds.key, trackerToken, trackerProjectId].forEach(function(dep) {
+  if (!dep) {
+    throw new Error("Check your Deps!")
+  }
+});
+
+var t = new Trello(trelloCreds.key, trelloCreds.token);
+if (!listsToTracker[current_state]) {
+  var message = 'missing trello board for' + current_state;
+  t.get('/1/boards/' + trelloCreds.boardId + '/lists', function(error, data) {
+    console.log(data);
+  });
+  console.log(message);
+}
+
+function createStory(json, callback) {
+  request({
     method: "POST",
-    json: story,
-    url: "https://www.pivotaltracker.com/services/v5/projects/"+project+"/stories",
+    json: json,
+    url: "https://www.pivotaltracker.com/services/v5/projects/" + trackerProjectId + "/stories",
     headers: {
-        'X-TrackerToken': 'abcd'
+      'X-TrackerToken': trackerToken
     }
   }, callback);
 }
 
-function writeToPivotal(trelloCards, callback){
-  async.each(trelloCards, function(item, cb){
-    // map pivotal: trello
+function writeToPivotal(trelloCards, callback) {
+  console.log('writing to pivotal: ', trelloCards.length);
+  async.each(trelloCards, function(item, cb) {
     createStory({
       name: item.name,
-      description: item.shortUrl + "  \n  "+ item.desc,
-      current_state: 'unstarted'
-    }, cb);
-
-  }, function(err){
-    if (err){
+      description: item.shortUrl + "  \n  " + item.desc,
+      current_state: current_state,
+      // estimate: 2, ESTIMATE REQUIRED FOR 'started' and 'finished'
+    }, function(error, response, body) {
+      if (error) {
+        console.log(error);
+        return cb(error)
+      }
+      console.log(body);
+      cb(null, body);
+    });
+  }, function(err) {
+    if (err) {
       callback(err)
     } else {
       callback(null, "Success")
@@ -34,35 +82,37 @@ function writeToPivotal(trelloCards, callback){
   });
 }
 
-var trelloCreds = {
-  // key: "abc",
-  // token: "abc",
-  // secret: 'abc'
-};
 
-var t = new Trello(trelloCreds.key, trelloCreds.token);
-var boardId = "fqvY2ld2";
-
-t.get('/1/boards/'+boardId+'/cards/', function(error, data){
-  var listTODOid = '546b4c23ac36eb3d928725cf';
-
-  // if (data == "abcd{
-    return new Error("data")
+return t.get('/1/boards/' + trelloCreds.boardId + '/cards/', function(error, data) {
+  if (data.indexOf("expired") > -1) {
+    throw new Error(data);
+    return
   }
+  var idList, filtered;
 
-  var filtered = _.filter(data, function(card){
-    return card.idList === listTODOid
+  idList = listsToTracker[current_state];
+
+  console.log('cards count:', data.length);
+
+  filtered = _.filter(data, function(card) {
+    return card.idList === idList;
   });
 
-  async.map(filtered, function(item, cb){
-    t.get('/1/boards/'+boardId+'/cards/'+item.id, cb)
-  }, function(error, result){
-    if (error){
-      return console.log(error);
-    }
-    writeToPivotal(result, function(err, res){
-      console.log(err)
-      console.log(res)
-    })
-  });
+  console.log(current_state + " count: ", filtered.length);
+
+  if (filtered && filtered.length > 0) {
+    async.map(filtered, function(item, cb) {
+      t.get('/1/boards/' + trelloCreds.boardId + '/cards/' + item.id, cb)
+    }, function(error, result) {
+      if (error) {
+        return console.log(error);
+      }
+      writeToPivotal(result, function(err, res) {
+        console.log("writeToPivotal err:", err);
+        console.log("writeToPivotal res:", res);
+      })
+    });
+  } else {
+    console.log('No Cards for current_state:[' + current_state + ']');
+  }
 });
